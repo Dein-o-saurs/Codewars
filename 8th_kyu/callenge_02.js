5d3d77d47f5773af9859ef35dfc3f349d2f42d1b
Max Prime <max4prim@gmail.com>
2022-05-13 16:57:44 +0300

    second challenge

---
+++ 8th_kyu/callenge_02.js
@@ -0,0 +1,8 @@
+// // Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+
+var stringToNumber = function(str){
+  
+    return parseInt(str);
+  }
