2520eb6e2837525f8b073a71a0dc0ee4da17d7c4
Max Prime <max4prim@gmail.com>
2022-05-13 16:46:57 +0300

    second codewars challenge

--- 8th_kyu/challenge_01.js
+++ 8th_kyu/challenge_01.js
@@ -1,4 +1,4 @@
-// Write a function called repeatStr which repeats the given string string exactly n times.
+// Write a function called repeatStr which repeats the given string string exactly n times. 1
 
 
 function repeatStr (n, s) {
