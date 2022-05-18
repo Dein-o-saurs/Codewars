0b6c1abb5a583207b30960588d28253688f04bc7
Max Prime <max4prim@gmail.com>
2022-05-18 20:04:37 +0300

    Sixth challenge

---
+++ 8th_kyu/challenge_06.js
@@ -0,0 +1,10 @@
+//Given an array of integers your solution should find the smallest integer.
+
+
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      let min = Math.min.apply(Math, args)
+      return min
+    }
+  }
