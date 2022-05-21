27153406805b1b3be6d394338174730d5b2b278d
Max Prime <max4prim@gmail.com>
2022-05-21 22:56:03 +0300

    ninth challenge

---
+++ 8th_kyu/challenge_09.js
@@ -0,0 +1,11 @@
+//Complete the square sum function so that it squares each number passed into 
+//it and then sums the results together.
+
+
+function squareSum(numbers){
+    let sum = 0;
+    numbers.forEach(function(n) {
+      sum += n * n
+    });
+    return sum;
+  }
