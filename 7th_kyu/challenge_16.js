f77f4e8f601656d30c7a3f22748a03f4af7c7732
Max Prime <max4prim@gmail.com>
2022-07-03 18:59:02 +0300

    sixteenth challenge, 7th kyu

---
+++ 7th_kyu/challenge_16.js
@@ -0,0 +1,9 @@
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+
+
+function minMax(arr){
+    const sorted = arr.sort((a,b) => a-b);
+    return [sorted[0], sorted[sorted.length-1]];
+  }
